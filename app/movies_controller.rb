1b57801c8b4f8989b1ef3458adc191d77e384447
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-09 22:23:44 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,26 +29,24 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    args.each do |a|
-      m.update(a)
-    end
+    m.update(args)
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
@@ -56,13 +54,13 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.where(title: "Title", release_date: 2000, director: "Me")[0]
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
